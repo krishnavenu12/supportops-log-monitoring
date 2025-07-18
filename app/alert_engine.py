6f4f2ba676c3e4ad2ca23d11982b2ae5c641e466
Krishna Venugopal <168347401+krishnavenu12@users.noreply.github.com>
2025-07-18 22:47:29 +0530

    Update alert_engine.py

--- app/alert_engine.py
+++ app/alert_engine.py
@@ -1,9 +1,7 @@
-alerts = []
+from app.database import fetch_logs
 
-def process_log(log):
-    if log.severity in ["ERROR", "CRITICAL"]:
-
-        alerts.append(log)
-
-def get_alerts():
-    return alerts
+# Returns only the critical alerts from logs
+def get_critical_alerts():
+    all_logs = fetch_logs()
+    critical_logs = [log for log in all_logs if log["severity"] == "CRITICAL"]
+    return critical_logs
